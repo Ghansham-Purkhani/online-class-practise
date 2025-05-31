7021a1fcc032c629b18083ff276f4a80b3306b31
GhanshamPurkhani <ghanshampurkhani79@gmail.com>
2025-05-30 17:04:20 -0700

    loop

--- 7th-class.js
+++ 7th-class.js
@@ -1,10 +1,40 @@
 /*
 slice and splice methood of array and loop 
 */
+//  // splice
+// let students=["gp","gd","raj"];
+// console.log(students);
+// students.splice(1,1,"sardar","Naresh");
+// console.log(students);
+// // slice
+// let studentClases = ["gp","gd","raj","sardar","Naresh"];
+// console.log(studentClases);
+// console.log(studentClases.slice(0,3));
+// console.log(studentClases.slice(3,5));
 
-/*💡 Question: array base
-Write a program that finds the largest number in an array of integers.
-*/
+// FOR LOOP IN JS
+// structure of for-loop
 
+for(var i=0;i<5;i++){
+console.log(i);
+}
+console.log(i); // there use rule of js  think what is output ?.
 
+                       // PalindromicWords
+
+let word = prompt("Enter a word:");
+word = word.toLowerCase(); // Make it case-insensitive
+let isPalindrome = true;
+let length = word.length;
+for (let i = 0; i < length / 2; i++) {
+    if (word[i] !== word[length - 1 - i]) {
+        isPalindrome = false;
+        break;
+    }
+} 
+if (isPalindrome) {
+    console.log("It's a palindrome!");
+} else {
+    console.log("It's not a palindrome.");
+}
 
