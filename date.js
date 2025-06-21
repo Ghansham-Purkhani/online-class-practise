d3971311b1703df34fb7a55513ceab1c9468a7a9
GhanshamPurkhani <ghanshampurkhani79@gmail.com>
2025-06-21 14:53:36 -0700

    date and its some methood

---
+++ date.js
@@ -0,0 +1,28 @@
+var newDate = new Date();
+var days = ["Sun","Mon","Tus","Wed","Thur","Fri","Sat"];
+console.log(days[newDate.getDay()]);
+
+// SPECIFING DATE AND TIME 
+var today = new Date();
+var domsDay= new Date("june 21,2035");
+
+console.log(today);
+console.log(domsDay);
+
+var mstoday = today.getTime();
+console.log(mstoday);
+
+var msdomsDay = domsDay.getTime();
+console.log(msdomsDay);
+
+var msDiff = msdomsDay - mstoday;
+console.log(msDiff);
+
+var dDiff = msDiff / (1000 * 60 * 60 * 24);
+console.log(dDiff);
+
+dDiff = Math.floor(dDiff);
+var msDiff = new Date("June 30, 2035").getTime() - new Date().getTime();
+var daysTillDoom = Math.floor(msDiff / (1000 * 60 * 60 * 24));
+var d = new Date("July 21, 1983 13:25:00");
+console.log(d);
