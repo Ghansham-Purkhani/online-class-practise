6e1bccea1320794c1aa8110060399e1adb61c376
GhanshamPurkhani <ghanshampurkhani79@gmail.com>
2025-06-23 23:26:24 +0500

    getTime methood

--- date.js
+++ date.js
@@ -25,15 +25,19 @@ var msDiff = new Date("June 30, 2035").getTime() - new Date().getTime();
 var daysTillDoom = Math.floor(msDiff / (1000 * 60 * 60 * 24));
 var d = new Date("July 21, 1983 13:25:00");
 console.log(d); */
-var newDate = new Date(); // MilliSecond 
-console.log(newDate.getTime());  
-var newTime = newDate/(1000);  // Second 
-console.log(newTime); 
-var newTime = newDate/(1000*60);  // Mintus
-console.log(newTime);
-var newTime = newDate/(1000*60*60);  // clock
-console.log(newTime);
-var newTime = newDate/(1000*60*60*24);  // day
-console.log(newTime);
+// var newDate = new Date(); // MilliSecond 
+// console.log("MilliSecond",newDate.getTime());  
+// var newTime = newDate/(1000);  // Second 
+// console.log("Second",newTime); 
+// var newTime = newDate/(1000*60);  // Mintus
+// console.log("Mintus",newTime);
+// var newTime = newDate/(1000*60*60);  // clock
+// console.log("Clock",newTime);
+// var newTime = newDate/(1000*60*60*24);  // day
+// console.log("Day",newTime);
+// var newTime = newDate/(1000*60*60*24*365);  // Year
+// console.log("Year",newTime); 
 
+var newDateMeth = new Date(1750692585898);
+console.log(newDateMeth.toLocaleString());
 
