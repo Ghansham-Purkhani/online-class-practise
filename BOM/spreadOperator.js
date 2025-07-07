5904e5c25e1b121a26a58735a2a44988cc339a35
GhanshamPurkhani <ghanshampurkhani79@gmail.com>
2025-07-08 02:02:28 +0500

    spread-operator

---
+++ BOM/spreadOperator.js
@@ -0,0 +1,20 @@
+//  spread operator = use for join array and obj
+// join array
+let Num1 = [1, 2, 3, 4, 5]
+let Num2 = [6, 7, 8, 9, 10]
+const joinNum1Num2 = [...Num1, ...Num2]
+console.log(joinNum1Num2)
+
+// join obj
+let objEct = {
+  firstname: 'Ghansham',
+  lastName: 'Purkhani',
+  age: 22
+}
+const objEctJoin = {
+  ...objEct,
+  city: 'Mithi',
+  Dist: 'Tharparker',
+  state: 'Sindh'
+}
+console.log(objEctJoin);
